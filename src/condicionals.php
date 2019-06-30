b731e5bdeb02a2ec37d327ef38cd34957b716c44
Jeff A <almanzarortizjeffrey@gmail.com>
2019-06-30 00:05:20 -0400

    learned conditioanls

---
+++ src/condicionals.php
@@ -0,0 +1,14 @@
+<?php
+
+//conditionals are the same as in JS
+$name = "Jeff";
+
+if($name === "Jose"){
+    echo "It's not cool.";
+}elseif($name ==="Jeff"){
+    echo "It's cool";
+}else{
+    echo "unknown";
+}
+
+?>
