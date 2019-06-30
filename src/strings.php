c58d7a419cb659c8273bd02362a2c105de43bf3b
Jeff A <almanzarortizjeffrey@gmail.com>
2019-06-29 23:36:34 -0400

    learned about strings

---
+++ src/strings.php
@@ -0,0 +1,11 @@
+<?php
+
+$name = "Jeff";
+$lastName = "Almanzar";
+
+$fullName = $name ." ". $lastName;
+echo $fullName;
+
+echo "\n use double string more than single one. also use the dot for concatenation."
+
+?>
