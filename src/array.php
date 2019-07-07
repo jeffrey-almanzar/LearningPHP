4a4870c5748332882006ea8f73d6684404a3a1df
Jeffrey-A <almanzarortizjeffrey@gmail.com>
2019-07-07 10:05:19 -0400

    learned arrays

---
+++ src/array.php
@@ -0,0 +1,50 @@
+<?php
+$a = array(
+    'name' => 'Jeff',
+    'lastName' => 'Almanzar'
+
+);
+
+//printing all elements in the array
+echo implode(', ', $a) . "\n";
+
+//size of the array
+echo count($a) . "\n";
+
+//appending to array
+$a['age'] = 22;
+echo implode(', ', $a) . "\n";
+
+//removing from array
+unset($a['age']);
+echo implode(', ', $a) . "\n";
+
+//sorting
+$people = ['Jeff', 'Ismael', 'Esmeralda', 'Arrianny', 'Beba'];
+echo "before sorting: \n";
+echo implode("\n ",$people);
+echo "after sorting: \n";
+sort($people);
+echo implode("\n ", $people);
+
+//reversing
+rsort($people);
+echo "\nReversing:";
+echo implode("\n",$people);
+
+//checking if a key exits
+echo "\n---------------------\n";
+echo array_key_exists("name", $a);
+
+//checking if a value exits
+echo "\n---------------------\n";
+echo in_array("Jeff", $people);
+
+//indexing
+echo "\n---------------------\n";
+echo $a['name'];
+echo "\n---------------------\n";
+echo $people[0]
+
+
+?>
