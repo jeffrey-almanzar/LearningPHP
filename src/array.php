a71200543c89f51e24ded7290fabe7ba46cdbec6
Jeffrey-A <almanzarortizjeffrey@gmail.com>
2019-07-07 11:36:02 -0400

    learned about array methods

--- src/array.php
+++ src/array.php
@@ -44,7 +44,22 @@ echo in_array("Jeff", $people);
 echo "\n---------------------\n";
 echo $a['name'];
 echo "\n---------------------\n";
-echo $people[0]
+echo $people[0];
 
+//more methods
+$anotherArr = array();
+//adding to the end
+array_push($anotherArr, 'Jeff');
+//adding to the beginning
+array_unshift($anotherArr,"Isma");
+
+//removing
+array_shift($anotherArr); //remove the first
+array_pop($anotherArr); //remove the last 
+
+/*
+unset removes key and value of an element, but it leaves a hole in the array
+so after unset use myArray = array_values(myArray);
+*/
 
 ?>
