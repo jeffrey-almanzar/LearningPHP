52357259f3f81a555711b07be5a84cc64413393f
Jeffrey-A <almanzarortizjeffrey@gmail.com>
2019-07-09 22:59:37 -0400

    learned loops syntax

---
+++ src/loops.php
@@ -0,0 +1,16 @@
+<?php
+//loop are the same as in other programs
+
+for($i =0; $i<10; $i++){
+    echo $i ." - ";
+    
+}
+echo "\n";
+$acc = 1;
+
+while($acc<11){
+    echo $acc . "\n";
+    $acc += 2;
+}
+
+?>
